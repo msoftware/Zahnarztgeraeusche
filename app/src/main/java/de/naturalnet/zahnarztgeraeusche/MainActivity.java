79b1dd0b4dc6d4ac1afd26235e0772851f9e67d0
Dominik George <nik@naturalnet.de>
2015-12-07 14:48:25 +0100

    First working version

--- app/src/main/java/de/naturalnet/zahnarztgeraeusche/MainActivity.java
+++ app/src/main/java/de/naturalnet/zahnarztgeraeusche/MainActivity.java
@@ -1,5 +1,7 @@
 package de.naturalnet.zahnarztgeraeusche;
 
+import android.app.Activity;
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
@@ -7,19 +9,78 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.ToggleButton;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private class SoundListAdapter extends BaseAdapter {
+        private ArrayList<String> items = new ArrayList<String>();
+        private LayoutInflater inflater;
+
+        public SoundListAdapter() {
+            inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        public void addItem(final String item) {
+            items.add(item);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public String getItem(int pos) {
+            return items.get(pos);
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return pos;
+        }
+
+        @Override
+        public View getView(int pos, View v, ViewGroup p) {
+            ViewHolder holder = null;
+            if (v == null) {
+                v = inflater.inflate(R.layout.atom_item, null);
+                holder = new ViewHolder();
+                holder.btn = (ToggleButton) v.findViewById(R.id.toggleButton);
+                v.setTag(holder);
+            } else {
+                holder = (ViewHolder) v.getTag();
+            }
+            holder.btn.setTextOn(items.get(pos));
+            holder.btn.setTextOff(items.get(pos));
+            holder.btn.setText(items.get(pos));
+            return v;
+        }
+
+        public class ViewHolder {
+            public ToggleButton btn;
+        }
+    }
+
+    SoundListAdapter adapter;
     Hashtable<String, MediaPlayer> players;
 
-    ToggleButton btnDrill;
+    ListView lstSounds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,29 +89,37 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        adapter = new SoundListAdapter();
         players = new Hashtable<String, MediaPlayer>();
 
         for (Field f : R.raw.class.getFields()) {
             try {
                 players.put(f.getName(), MediaPlayer.create(this, f.getInt(f)));
+                players.get(f.getName()).setLooping(true);
+                adapter.addItem(f.getName());
             } catch (IllegalAccessException e) {
                 // FIXME Do something useful
             }
         }
 
-        btnDrill = (ToggleButton) findViewById(R.id.btnDrill);
-        btnDrill.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (btnDrill.isChecked()) {
-                    players.get("drill").setLooping(true);
-                    players.get("drill").start();
-                } else {
-                    players.get("drill").pause();
-                    players.get("drill").seekTo(0);
-                }
-            }
-        });
+        lstSounds = (ListView) findViewById(R.id.lstSounds);
+        lstSounds.setAdapter(adapter);
+    }
+
+    public void btnSoundOnClickHandler(View v) {
+        ToggleButton btn = (ToggleButton) v.findViewById(R.id.toggleButton);
+        toggleSound(btn.getTextOn().toString());
+    }
+
+    public void toggleSound(String name) {
+        MediaPlayer player = players.get(name);
+
+        if (! player.isPlaying()) {
+            player.start();
+        } else {
+            player.pause();
+            player.seekTo(0);
+        }
     }
 
     @Override
